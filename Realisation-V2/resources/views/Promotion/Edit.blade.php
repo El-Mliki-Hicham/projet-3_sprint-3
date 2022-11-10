cba6ad738b258ec7f9ab6ecd1152ff11242f4a4c
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-11-10 22:48:00 +0100

    style

--- Realisation-V2/resources/views/Promotion/Edit.blade.php
+++ Realisation-V2/resources/views/Promotion/Edit.blade.php
@@ -1,62 +1,90 @@
-@include("layouts.head")
+@extends("Master")
+@section('content')
+
+<div class="container-fluid py-4">
+
+
+    @foreach ($brief as $brief )
+
+   <h1>Brief assigner :</h1> <h2> {{$brief->Nom_du_brief}} </h2>
+    @endforeach
 
 
 
+    <div class="row">
+      <div class="col-12">
+        <div class="card my-4">
+          <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+            <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+              <h6 class="text-white text-capitalize ps-3">
 
-<form method="POST" action="{{route("promotion.update",$promotion->id)}}">
-    @method("PUT")
-    @csrf
-<input type="text" value="{{$promotion->Name_promotion}}" name="Name">
-<button>Update</button>
-</form>
+                <form method="POST" action="{{route("promotion.update",$promotion->id)}}">
+                    @method("PUT")
+                    @csrf
+                <input type="text" value="{{$promotion->Name_promotion}}" name="Name">
+                <button>Update</button>
+                </form>
 
-<br>
-<br>
 
-@foreach ($brief as $brief )
+                <div class="input-search-briefs">
+                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                        <div class="input-group input-group-outline inputsearch">
+                          <label class="form-label">Type here...</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+              </div>
+              </h6>
+            </div>
+          </div>
+          <div class="card-body px-0 pb-2">
+            <div class="table-responsive p-0">
+              <table class="table align-items-center justify-content-center mb-0">
+                <thead>
+                  <tr>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Id</th>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">First name</th>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Last name</th>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Email</th>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">Action</th>
 
-<h2>{{$brief->Nom_du_brief}} </h2>
-@endforeach
-<br><br>
-
-<button class="btn btn-primary" ><a href="{{route('student.create',$id)}}" style="color: white">add student</a></button>
-<br>
-<br>
-
-<table class="table-bordered">
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Email</th>
-            <th>promotion id</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach ($student as $item )
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach ($student as $item )
         <tr>
             <td>{{$item->id}}</td>
             <td>{{$item->First_name}}</td>
             <td>{{$item->Last_name}}</td>
             <td>{{$item->Email}}</td>
-            <td>{{$item->promotion_id}}</td>
-            <td class="d-flex"><a href="{{route("student.edit",$item->id)}}" class="btn btn-success">Edit</a>
+                    <td class="td-btn" >
+                        <a href="{{route('student.edit',$item->id)}}" class="" style="color: green" title="Edit"><i class="fa-regular fa-pen-to-square"></i></a>
 
-            <form action="{{route("student.destroy",$item->id)}}" method='post'>
-                @method("DELETE")
-                @csrf
-           <button class="btn btn-danger" > Delete</button>
-        </form>
-        </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+                        <form action="{{route('student.destroy',$item->id)}}" method="POST">
+                            @method("DELETE")
+                            @csrf
+                            <button class="delete" style="all: unset;cursor: pointer;color:red" title="Delete" data-toggle="tooltip"><i class="fa-solid fa-trash"></i></button>
 
+                        </form>
+                        <button class="btn btn-info" >  <a style="color: white" href="{{route('assigner.show',$item->id)}}">assigner</a></button>
+                    </td>
 
-<button><a href="{{route('promotion.index')}} ">return</a></button>
+                  </tr>
+                  @endforeach
 
 
 
+                </tbody>
+              </table>
+              <button><a href="{{route('promotion.index')}} ">return</a></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+
+    @endsection
--- Realisation-V2/resources/views/Promotion/index.blade.php
+++ Realisation-V2/resources/views/Promotion/index.blade.php
@@ -1,31 +1,76 @@
-@include("layouts.head")
-@include("welcome")
+@extends("Master")
+@section('content')
 
-<br><br>
-<a href="{{route("promotion.create")}}"><button>add promotion</button></a>
-<br><br>
-<table class="table-bordered" width="500">
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Name promotion</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach  ($promotion as $item )
-        <tr>
-            <td>{{$item->id}}</td>
-            <td>{{$item->Name_promotion}}</td>
-            <td class="d-flex"><a class="btn btn-success" href="{{route("promotion.edit",$item->id)}}">Edit</a>
+<div class="container-fluid py-4">
 
-            <form action="{{route("promotion.destroy",$item->id)}}" method='post'>
-                @method("DELETE")
-                @csrf
-           <button class="btn btn-danger" style="margin-left"> Delete</button>
-        </form>
-        </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+
+
+
+
+
+    <div class="row">
+      <div class="col-12">
+        <div class="card my-4">
+          <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+            <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+              <h6 class="text-white text-capitalize ps-3">
+
+                <a href="{{route("promotion.create")}}"><button class="btn btn-warning">Ajouter promotion</button></a>
+
+                <div class="input-search-briefs">
+                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                        <div class="input-group input-group-outline inputsearch">
+                          <label class="form-label">Type here...</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+              </div>
+              </h6>
+            </div>
+          </div>
+          <div class="card-body px-0 pb-2">
+            <div class="table-responsive p-0">
+              <table class="table align-items-center justify-content-center mb-0">
+                <thead>
+                  <tr>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">id</th>
+                    <th class="text-uppercase text-secondary text-xxs  font-weight-bolder opacity-7"> Nom promotion</th>
+                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder text-center opacity-7 ps-2">action</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach  ($promotion as $item )
+                    <tr>
+                        <td>{{$item->id}}</td>
+                        <td>{{$item->Name_promotion}}</td>
+                    <td class="td-btn" >
+                        <a href="{{route('promotion.edit',$item->id)}}" class="" style="color: green" title="Edit"><i class="fa-regular fa-pen-to-square"></i></a>
+
+                        <form action="{{route('promotion.destroy',$item->id)}}" method="POST">
+                            @method("DELETE")
+                            @csrf
+                            <button class="delete" style="all: unset;cursor: pointer;color:red" title="Delete" data-toggle="tooltip"><i class="fa-solid fa-trash"></i></button>
+
+                        </form>
+                        <button class="btn btn-info" >  <a style="color: white" href="{{route('assigner.show',$item->id)}}">assigner</a></button>
+                    </td>
+
+                  </tr>
+                  @endforeach
+
+
+
+                </tbody>
+              </table>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+
+    @endsection
